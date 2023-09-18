d475daabaf2099bb872b2f1ae527c4420084dc7d
mrpeepernumber007 <federico.e.naso@gmail.com>
2023-09-18 16:02:56 -0300

    Create basic calculator

--- app.js
+++ app.js
@@ -4,31 +4,41 @@ const operator = ''
 
 
 function add (a, b) {
-    return a + b
+    return console.log(a+b);
 }
 function subtract (a, b) {
-    return a - b
+    return console.log(a-b);
 }
 function multiply (a, b) {
-    return a * b
+    return console.log(a*b);
 }
 function divide (a, b) {
-    return a / b
+    return console.log(a/b);
 }
 
 function operate(n1, n2, opt) {
     switch (opt) {
-        case add:
+        case 'add':
             add(n1, n2)
             break;
-        case subtract:
+        case 'subtract':
             subtract(n1, n2)
             break;
-        case multiply:
+        case 'multiply':
             multiply(n1, n2)
             break;
-        case divide:
-            div(n1, n2)
+        case 'divide':
+            divide(n1, n2)
             break;
     }
 }
+
+function calculator () {
+    const number1 = +prompt('First number: ')
+    const number2 = +prompt('Second number: ')
+    const operation = prompt('Operation: ')
+    operate(number1, number2, operation)
+}
+
+const btn = document.querySelector('.btn')
+btn.addEventListener('click', calculator)
